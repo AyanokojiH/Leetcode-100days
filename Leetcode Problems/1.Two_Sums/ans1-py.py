5d8b77738cf1d638bfcaa21d02fa1681816c284f
AyanokojiH <161097924+AyanokojiH@users.noreply.github.com>
2025-05-28 20:17:19 +0800

    Delete Leetcode Problems/1.Two_Sums directory

--- Leetcode Problems/1.Two_Sums/ans1-py.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        hash = {}
-        for idx, num in enumerate(nums):
-            complement = target - num
-            if complement in hash:
-                return [hash[complement], idx]
-            hash[num] = idx  # 先检查再存入，避免同一元素重复使用
-        return []
